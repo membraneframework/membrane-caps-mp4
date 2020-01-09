b9024746e19a8255d8b7aaf2943b44102f3e9fae
Mateusz Front <mateusz.front@swmansion.com>
2020-01-09 19:25:41 +0100

    add inter_frames? field

--- lib/membrane_caps_mp4_payload.ex
+++ lib/membrane_caps_mp4_payload.ex
@@ -1,4 +1,4 @@
 defmodule Membrane.Caps.MP4.Payload do
   @enforce_keys [:content_type, :sample_duration, :timescale]
-  defstruct @enforce_keys ++ [height: 0, width: 0, type_specific: %{}]
+  defstruct @enforce_keys ++ [height: 0, width: 0, type_specific: %{}, inter_frames?: false]
 end
