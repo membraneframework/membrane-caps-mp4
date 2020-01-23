7c2e1c3cf5527b70e25c872f09cbf70b7d3b5825
Mateusz Front <mateusz.front@swmansion.com>
2020-01-23 16:38:10 +0100

    refactor

---
+++ lib/membrane_caps_mp4/payload/avc1.ex
@@ -0,0 +1,4 @@
+defmodule Membrane.Caps.MP4.Payload.AVC1 do
+  @enforce_keys [:avcc]
+  defstruct @enforce_keys
+end
--- lib/membrane_caps_mp4_payload.ex
+++
@@ -1,4 +0,0 @@
-defmodule Membrane.Caps.MP4.Payload do
-  @enforce_keys [:content_type, :sample_duration, :timescale]
-  defstruct @enforce_keys ++ [height: 0, width: 0, type_specific: %{}, inter_frames?: false]
-end
