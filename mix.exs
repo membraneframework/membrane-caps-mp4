779b83321be8435898759ac8d26046f1d7bf4458
Michał Śledź <michal.sledz@swmansion.com>
2022-05-13 12:28:33 +0200

    Bump version to 0.7.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.MP4.Format.MixProject do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.7.0"
   @github_url "https://github.com/membraneframework/membrane_mp4_format"
 
   def project do
